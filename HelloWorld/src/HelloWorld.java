d00b7870faacf993785a724a775ad970e89dd78e
vibeseren <139480528+vibeseren@users.noreply.github.com>
2023-10-14 18:57:06 +0200

    seveneight

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("ganzNeu");
 		System.out.println("helloworld");
+		System.out.println("das ist auch neu");
 
 	}
 
