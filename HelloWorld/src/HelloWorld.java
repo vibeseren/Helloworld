c05b49d798f8b98a20303520694f60b98464686a
vibeseren <139480528+vibeseren@users.noreply.github.com>
2023-10-14 19:05:46 +0200

    sevennine

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 		System.out.println("ganzNeu");
 		System.out.println("helloworld");
 		System.out.println("das ist auch neu");
+		System.out.println("erenistcool");
+		System.out.println("eclipsetogithub");
 
 	}
 
